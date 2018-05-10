9a6e91eda13a0c972fbdeccebe234f3f9c73258a
hedleyroos <hedleyroos@gmail.com>
2018-05-10 17:22:14 +0200

    Add app_name

--- simple_autocomplete/urls.py
+++ simple_autocomplete/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from simple_autocomplete.views import get_json
 
 
+app_name = "simple_autocomplete"
 urlpatterns = [
     url(r'^(?P<token>[\w-]+)/$', get_json, name='simple-autocomplete')
 ]
